e330f58ab10a1586d1f7234300b698ad420038cb
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-25 17:08:50 -0300

    feat: refatora todas as raças pra facilitar instanciação com reuso da classe abstrata races

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -3,16 +3,11 @@ import Race from './Race';
 class Dwarf extends Race {
   lifePoints: number;
   constructor(
-    readonly _name: string,
     readonly _dexterity: number,
   ) {
-    super('Dwarf', 80);
+    super('Dwarf', _dexterity);
     this.lifePoints = 80;
   }
-
-  get name() { return this._name; }
-
-  get dexterity() { return this._dexterity; }
 
   get maxLifePoints(): number {
     return this.lifePoints;
